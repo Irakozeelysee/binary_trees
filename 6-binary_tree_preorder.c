35fd60b0d3508ec3d3efad93571465c440d5638f
Shalom Ogoziem <shalomzymike@gmail.com>
2023-08-29 07:50:13 +0000

    remove invalid comment type

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -10,13 +10,13 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	// call the function on the current node's value
+	/* call the function on the current node's value */
 	func(tree->n);
 
-	// traverse the left subtree
+	/* traverse the left subtree */
 	binary_tree_preorder(tree->left, func);
 
-	// traverse the right subtree
+	/* traverse the right subtree */
 	binary_tree_preorder(tree->right, func);
 
 }
