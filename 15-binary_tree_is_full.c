c64f14e45a23696e39b99e0a982271104328076a
Shalom Ogoziem <shalomzymike@gmail.com>
2023-08-29 10:15:13 +0000

    check if tree is perfect

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,11 +10,14 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
+	/* if both children are null, it is a leaf i.e it is full */
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
+	/* if one child is null and the other is not null it is not full */
 	if (tree->left == NULL || tree->right == NULL)
 		return (0);
 
+	/* recursively check left and right subtrees */
 	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 }
