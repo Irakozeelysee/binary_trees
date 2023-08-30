06848aa85b06d80dc6c4019001fa16005b1ca3f1
Shalom Ogoziem <shalomzymike@gmail.com>
2023-08-30 13:22:09 +0000

    fix compilation flag requirement

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -13,9 +13,10 @@
 binary_tree_t *binary_trees_ancestor(
 		const binary_tree_t *first, const binary_tree_t *second)
 {
+	binary_tree_t *ancestor = (binary_tree_t *)first;
+
 	if (first == NULL || second == NULL)
 		return (NULL);
-	binary_tree_t *ancestor = (binary_tree_t *)first;
 
 	while (ancestor != NULL)
 	{
